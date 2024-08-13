88845546b71018a3c9ec90812d667935b58bbc64
KARTHIK S <karthikkichu153@gmail.com>
2024-08-13 11:23:06 +0530

    Add files via upload

---
+++ LAB/reparray.c
@@ -0,0 +1,16 @@
+//REPRESENTATION OF POLYNOMIAL USING ARRAY
+#include <stdio.h>
+
+int main()
+
+{
+
+    int p1[3] = {6,2,5};
+
+    for(int i=2;i>=0;i--){
+
+        printf("%dx^%d+",p1[i],i);
+
+    }
+
+}
